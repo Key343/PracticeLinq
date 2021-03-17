2d426c27e20acf80aec0bd7ac1f9a4cb6f30944a
Key343 <keyonna.sharde@gmail.com>
2021-03-16 20:07:59 -0400

    initial commit

---
+++ SelectionStatements/SelectionStatements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favNumber;
+            favNumber = 3;
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < 3)
+            {
+                Console.WriteLine("Too low");
+            }
+            else if (userInput > 3) 
+            {
+                Console.WriteLine("Too High");
+            }
+            else { Console.WriteLine(" Nevermind "); }
+
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
