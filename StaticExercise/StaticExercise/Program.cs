27b52fad743ccab8bcef65c15be021b8d1b8b64a
Key343 <keyonna.sharde@gmail.com>
2021-04-02 14:37:50 -0400

    initial commit

---
+++ StaticExercise/StaticExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conversion = new TempConverter();
+            Console.WriteLine(conversion.FahrenheitToCelsius(32));
+            Console.WriteLine(conversion.CelsiusToFahrenheit(0));
+        }
+    }
+}
---
+++ StaticExercise/StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace StaticExercise
+{
+    public class TempConverter
+    {
+        public TempConverter()
+        {
+        }
+        public double FahrenheitToCelsius(double temp)
+        {
+            var cel = (temp - 32) / 1.8;
+                return cel;
+        }
+        public double CelsiusToFahrenheit(double temp)
+        {
+            var far =(temp * 1.8) + 32;
+            return far;
+        }
+    }
+}
