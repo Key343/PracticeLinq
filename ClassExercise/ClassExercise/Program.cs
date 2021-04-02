0690edadc975d8aa91965e94585aaef94daf9c4a
Key343 <keyonna.sharde@gmail.com>
2021-04-02 14:58:27 -0400

    initial commit

---
+++ ClassExercise/ClassExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Toyota = new Car();
+
+            Toyota.Make = "Chevrolet";
+            Toyota.Model = "Camaro";
+            Toyota.Year = 2009;
+
+
+            Console.WriteLine(Toyota.Make);
+            Console.WriteLine(Toyota.Model);
+            Console.WriteLine(Toyota.Year);
+
+
+
+        }
+    }
+}
