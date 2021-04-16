11c67092d81ca7d8c5048e98a15a3fc7211a499c
Key343 <keyonna.sharde@gmail.com>
2021-04-16 19:34:46 -0400

    initial commit

---
+++ PracticeLinq/PracticeLinq/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string>();
+
+            videoGames.Add("SpiderMan");
+            videoGames.Add("Arkham Knight");
+            videoGames.Add("2k21");
+            videoGames.Add("Madden");
+
+            var gameNameLength = videoGames.OrderBy(x => x.Length);
+
+            foreach (var num in gameNameLength)
+            {
+                Console.WriteLine(num);
+            }
+
+
+
+
+
+        }
+    }
+}
